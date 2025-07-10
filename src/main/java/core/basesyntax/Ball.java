fcf6eb315a14911d343717af95615943eb2c335a
Andrii Honchar <andriihoncharw@gmail.com>
2025-07-10 16:22:32 +0300

    implemented ColorSupplier method and created Lottery class

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -5,11 +5,20 @@ public class Ball {
     private int number;
 
 
+
     @Override
     public String toString() {
         return "Ball{" +
                 "color='" + color + '\'' +
                 ", number=" + number +
                 '}';
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
     }
 }
